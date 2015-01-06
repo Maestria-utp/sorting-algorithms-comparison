85cd1d83e43e9207f9f6d8750a563469f92b0699
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2015-01-05 19:14:23 -0500

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sortinAlgorithmsComparisonApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sortinAlgorithmsComparisonApp
+ */
+angular.module('sortinAlgorithmsComparisonApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
