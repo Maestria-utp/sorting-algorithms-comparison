cf70b63cc5ef128e5008a3fef1e783b90c5acb57
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2015-01-05 20:35:28 -0500

    insertion sort implemented

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+* @ngdoc function
+* @name sortinAlgorithmsComparisonApp.controller:MainCtrl
+* @description
+* # AboutCtrl
+* Controller of the sortinAlgorithmsComparisonApp
+*/
+angular.module('sortinAlgorithmsComparisonApp')
+.controller('AboutCtrl', function ($scope) {
+  
+});
