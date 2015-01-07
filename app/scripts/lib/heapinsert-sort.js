baa4b68b1caddca782145b40f6ff207a40f1eed2
sanrodari <sanrodari@gmail.com>
2015-01-07 17:13:04 -0500

    HeapinsertSort implementatio

---
+++ app/scripts/lib/heapinsert-sort.js
@@ -0,0 +1,85 @@
+'use strict';
+
+(function (sorting, undefined) {
+
+// Artículo http://revistas.utp.edu.co/index.php/revistaciencia/article/view/251
+// Hugo Humberto Morales Peña, Angel Augusto Agudelo, Jorge Ivan Rios Patiño
+
+// function HeapinsertSort(A)
+// 1. BuildHeapMax(A)
+// 2. for i <- 1 to floor(lenght(A) / 2)
+// 3.   do A[i] <-> A[length[A] − i + 1]
+// 4. InsertionSort(A)
+
+// function BuildMaxHeap(A)
+// 1.  heapSize[A] <- size[A] // Esta asignación se usa después, es una asignación global.
+// 2.  for i <- floor(size[A] / 2) downto 1
+// 3.    MaxHeapify(A, i)
+
+// function MaxHeapify(n, i)
+// 1.  l <- Left(i)
+// 2.  r <- Right(i)
+// 3.  if l <= heapSize[A] and A[l] > A[i] then
+// 4.    largest <- l
+// 5.  else
+// 6.    largest <- i
+// 7.  if r <= heapSize[A] and A[r] > A[largest] then
+// 8.    largest <- r
+// 9.  if largest != i then
+// 10.   exchange A[i] <-> A[largest]
+// 11.   MaxHeapify(A, largest)
+
+  sorting.buildMaxHeap = function (A) {
+    var heapSize = A.length;
+    for (var i = Math.floor(A.length / 2) ; i >= 0; i--) {
+      maxHeapify(A, i, heapSize);
+    };
+  };
+
+  var parent = function (n) {
+    return Math.floor(n / 2);
+  };
+
+  var left = function (n) {
+    return 2 * n + 1;
+  };
+
+  var right = function (n) {
+    return 2 * n + 2;
+  };
+
+  var maxHeapify = function (A, i, heapSize) {
+    var
+      l = left(i),
+      r = right(i),
+      largest = 0;
+
+    if (l <= heapSize && A[l] > A[i]) {
+      largest = l;
+    } else {
+      largest = i;
+    }
+    if (r <= heapSize && A[r] > A[largest]) {
+      largest = r;
+    }
+    if (largest !== i) {
+      var tmp = A[i];
+      A[i] = A[largest];
+      A[largest] = tmp;
+      maxHeapify(A, largest, heapSize);
+    }
+  };
+
+  sorting.heapinsertSort = function (array) {
+    sorting.buildMaxHeap(array);
+    for (var i = 0; i < Math.floor(array.length / 2); i++) {
+      // Se corrige el 0 index accediendo el elemento en [array.length - i - 1]
+      var tmp = array[i];
+      array[i] = array[array.length - i - 1];
+      array[array.length - i - 1] = tmp;
+    };
+    sorting.byInsertion(array);
+    console.log('arreglo ordenado')
+  };
+
+}( window.sorting = window.sorting || {} ));
