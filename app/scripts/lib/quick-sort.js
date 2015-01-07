3812936b5d204c05f12dff015b7f09f693909c1f
sanrodari <sanrodari@gmail.com>
2015-01-07 09:06:41 -0500

    QuickSort and test utilities

---
+++ app/scripts/lib/quick-sort.js
@@ -0,0 +1,53 @@
+'use strict';
+
+(function (sorting, undefined) {
+
+// function QuickSort(array, pStart, rEnd)
+// 1.  if pStart < rEnd then
+// 2.    indexPartition <- Partition(array, pStart, rEnd)
+// 3.    QuickSort(array, pStart, indexPartition - 1)
+// 4.    QuickSort(array, indexPartition + 1, rEnd)
+
+// function Partition(array, pStart, rEnd)
+// 1.  pivot <- array[rEnd]
+// 2.  i <- pStart - 1
+// 3.  for j <- pStart to rEnd - 1 do
+// 4.    if array[j] <= pivot then
+// 5.      i <- i + 1
+// 6.      exchange array[i] <-> array[j]
+// 7.  exchange array[i+1] <-> array[rEnd]
+// 8. return i+1
+
+  var partition = function (array, pStart, rEnd) {
+    var
+      pivot = array[rEnd],
+      i = pStart - 1;
+    for (var j = pStart; j < rEnd; j++) {
+      if(array[j] <= pivot) {
+        i++;
+        var tmp = array[i];
+        array[i] = array[j];
+        array[j] = tmp;
+      }
+    };
+    var tmp = array[i + 1];
+    array[i + 1] = array[rEnd];
+    array[rEnd] = tmp;
+
+    return i + 1;
+  };
+
+  var quickSortOriginal = function (array, pStart, rEnd) {
+    if (pStart < rEnd) {
+      var indexPartition = partition(array, pStart, rEnd);
+      quickSortOriginal(array, pStart, indexPartition - 1);
+      quickSortOriginal(array, indexPartition + 1, rEnd);
+    }
+  };
+
+  sorting.quickSort = function (array) {
+    quickSortOriginal(array, 0, array.length - 1);
+    console.log('Arreglo ordenado');
+  };
+
+}( window.sorting = window.sorting || {} ));
