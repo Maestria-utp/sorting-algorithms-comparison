853ef5048ace0c98a739bf385f13ad1d01fa93f2
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2015-01-07 20:09:33 -0500

    improve the format in the comment of insertion sort

--- app/scripts/lib/insertion-sort.js
+++ app/scripts/lib/insertion-sort.js
@@ -5,7 +5,9 @@
   sorting.testArray = [4, 5, 2, 10, 25, 0, 12, 5];
 
   /**
+  *
   * Sort an array of integers by insertion, this algorithm have an order of O(n^2)
+  *
   * @param {array} unsortedArray to be sorted
   * @return {array} sorted array
   */
