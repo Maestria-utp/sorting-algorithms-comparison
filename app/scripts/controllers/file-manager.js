f0b48610b308321d2302b59ba132047a6fea9147
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2015-01-10 01:25:07 -0500

    generating arrays

--- app/scripts/controllers/file-manager.js
+++ app/scripts/controllers/file-manager.js
@@ -4,7 +4,6 @@ angular.module('sortinAlgorithmsComparisonApp')
 .controller('FileManagerCtrl', function ($scope) {
   $scope.tab = 1;
   $scope.arrays = arrays.loadedArrays;
-  $scope.arrayFile;
 
   $scope.activeTab = function(tab) {
     return tab === $scope.tab;
@@ -19,6 +18,7 @@ angular.module('sortinAlgorithmsComparisonApp')
     var newArray = JSON.parse($scope.arrayFile);
     arrays.loadedArrays.push(newArray);
     $scope.tab = 1;
+    $scope.arrayFile = undefined;
     messagesManager.addMessage('info', 'El arreglo ha sido cargado.');
   };
 
@@ -26,7 +26,21 @@ angular.module('sortinAlgorithmsComparisonApp')
     $scope.arrayFile = content;
   };
 
-  $scope.generateArray = function() {
+  $scope.generateArray = function(size, max) {
+    var newArray = [];
+    console.log('inicio de generacion de arreglo, size: ' + size + ', maxNumber: ' + max);
+    for (var i = 0; i < size; i++) {
+      newArray.push(Math.floor(Math.random() * max));
+    }
+    console.log('fin de la generacion del arreglo');
+    var object = {
+      size: size,
+      maxNumber: max,
+      value: newArray
+    };
+
+    arrays.loadedArrays.push(object);
+    $scope.tab = 1;
     messagesManager.addMessage('info', 'El arreglo ha sido creado.');
   };
 });
